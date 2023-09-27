3b40958428dc39e190e54a5d52af341d2f2a0d23
fikretzeybek <fzeybek1972@gmail.com>
2023-09-28 00:01:05 +0300

    ek

---
+++ src/Deneme/Hello.java
@@ -0,0 +1,16 @@
+package Deneme;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Sanada merhaba");
+    }
+
+    public static  String str(){
+
+     String   str="d";
+
+return str;
+    }
+}
